da94d0a31e8efbc8266aec29f173e7181a61406b
wuyougongzi <1211385701@qq.com>
2016-05-16 22:04:07 +0800

    添加go接口学习示例，go的接口比较简单，舍弃了继承的方式，抽象出接口，只要实现了接口就能实现类似多态的效果

---
+++ interface.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	raduis float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.raduis * c.raduis
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.raduis
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func main() {
+	r := rect{10, 2}
+	c := circle{4}
+
+	measure(r)
+	measure(c)
+}
