18a0ab1038650c4307ad7f83b7777a7d157ff856
wuyougongzi <1211385701@qq.com>
2016-06-07 23:12:05 +0800

    添加go 公共函数处理包，添加checkerr检查函数

--- src/gotesting/mysql_test.go
+++ src/gotesting/mysql_test.go
@@ -4,20 +4,15 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"goutility"
 	"testing"
 )
-
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
 
 func TestMysql(t *testing.T) {
 	fmt.Println("connect to db")
 
 	db, err := sql.Open("mysql", "root:123@/testdb")
-	checkErr(err)
+	goutility.CheckErr(err)
 
 	rows, err := db.Query("select * from person")
 
@@ -27,7 +22,7 @@ func TestMysql(t *testing.T) {
 		var id string
 
 		err = rows.Scan(&name, &age, &id)
-		checkErr(err)
+		goutility.CheckErr(err)
 		fmt.Println(" name ", " age ", "     id       ")
 		fmt.Println(name, age, id)
 	}
---
+++ src/goutility/goutility.go
@@ -0,0 +1,7 @@
+package goutility
+
+func CheckErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
